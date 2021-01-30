50eaa8b919232a92f886b890424e68cd902f3a2a
Thành Nguyễn (Software Engineer II) <thanh.nguyen3@be.com.vn>
2021-01-30 18:15:56 +0000

    update readme 36

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "start git"
-export GIT_AUTHOR_DATE="Sat, 30 Jan 2021 18:05:56 -0000"
-export GIT_COMMITTER_DATE="Sat, 30 Jan 2021 18:05:56 -0000"
+export GIT_AUTHOR_DATE="Sat, 30 Jan 2021 18:15:56 -0000"
+export GIT_COMMITTER_DATE="Sat, 30 Jan 2021 18:15:56 -0000"
 git add .
-git commit -m "update readme 35"
+git commit -m "update readme 36"
 exit 0
