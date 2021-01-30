57889ba7de29d43cf7f379b89f5b8ef8d8fb873e
Thành Nguyễn (Software Engineer II) <thanh.nguyen3@be.com.vn>
2021-01-30 12:15:56 +0000

    update readme 0

---
+++ test.py
@@ -0,0 +1,24 @@
+import time;
+from datetime import date
+from email.utils import formatdate
+import subprocess
+
+f = open('readme.md', 'a')
+
+
+for i in range(0, 1500):
+    print(f'start {i}')
+    f.write(f'test {i} \n')
+    curTime = formatdate(1612008956 + i * 600)
+    f2 = open('test.sh', 'w')
+    f2.write(f'#!/bin/sh\n')
+    f2.write(f'echo \"start git\"\n')
+    f2.write(f'export GIT_AUTHOR_DATE=\"{curTime}\"\n')
+    f2.write(f'export GIT_COMMITTER_DATE=\"{curTime}\"\n')
+    f2.write(f'git add .\n')
+    f2.write(f'git commit -m \"update readme {i}\"\n')
+    f2.write(f'exit 0')
+    f2.close()
+
+    process = subprocess.Popen(['sh', './test.sh'])
+    process.wait()
