36bc80bbb961b6c5459d199642a5edd89234b19e
Thành Nguyễn (Software Engineer II) <thanh.nguyen3@be.com.vn>
2021-02-05 20:25:56 +0000

    update readme 913

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "start git"
-export GIT_AUTHOR_DATE="Fri, 05 Feb 2021 20:15:56 -0000"
-export GIT_COMMITTER_DATE="Fri, 05 Feb 2021 20:15:56 -0000"
+export GIT_AUTHOR_DATE="Fri, 05 Feb 2021 20:25:56 -0000"
+export GIT_COMMITTER_DATE="Fri, 05 Feb 2021 20:25:56 -0000"
 git add .
-git commit -m "update readme 912"
+git commit -m "update readme 913"
 exit 0
