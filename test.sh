c5a81ae713dbe64ddd770e86c255d48e405b31a5
thanhnc58 <nc.thanh1995@gmail.com>
2021-04-04 01:27:35 +0000

    update readme 365

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "start git"
-export GIT_AUTHOR_DATE="Sun, 04 Apr 2021 01:17:35 -0000"
-export GIT_COMMITTER_DATE="Sun, 04 Apr 2021 01:17:35 -0000"
+export GIT_AUTHOR_DATE="Sun, 04 Apr 2021 01:27:35 -0000"
+export GIT_COMMITTER_DATE="Sun, 04 Apr 2021 01:27:35 -0000"
 git add .
-git commit -m "update readme 364"
+git commit -m "update readme 365"
 exit 0
