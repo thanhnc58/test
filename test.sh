60241fb80dabb51e41b2893ff5ed5532a8d413a2
thanhnc58 <nc.thanh1995@gmail.com>
2021-04-04 07:47:35 +0000

    update readme 403

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "start git"
-export GIT_AUTHOR_DATE="Sun, 04 Apr 2021 07:37:35 -0000"
-export GIT_COMMITTER_DATE="Sun, 04 Apr 2021 07:37:35 -0000"
+export GIT_AUTHOR_DATE="Sun, 04 Apr 2021 07:47:35 -0000"
+export GIT_COMMITTER_DATE="Sun, 04 Apr 2021 07:47:35 -0000"
 git add .
-git commit -m "update readme 402"
+git commit -m "update readme 403"
 exit 0
