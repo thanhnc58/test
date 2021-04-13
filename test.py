438bb7ffc58123da34bef21f0c0a5093cf9da760
thanhnc58 <nc.thanh1995@gmail.com>
2021-04-13 12:37:35 +0000

    update readme 0

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ f = open('readme.md', 'a')
 for i in range(0, 1500):
     print(f'start {i}')
     f.write(f'test {i} \n')
-    curTime = formatdate(1617280655 + i * 600)
+    curTime = formatdate(1618317455 + i * 300)
     f2 = open('test.sh', 'w')
     f2.write(f'#!/bin/sh\n')
     f2.write(f'echo \"start git\"\n')
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "start git"
-export GIT_AUTHOR_DATE="Sun, 11 Apr 2021 22:27:35 -0000"
-export GIT_COMMITTER_DATE="Sun, 11 Apr 2021 22:27:35 -0000"
+export GIT_AUTHOR_DATE="Tue, 13 Apr 2021 12:37:35 -0000"
+export GIT_COMMITTER_DATE="Tue, 13 Apr 2021 12:37:35 -0000"
 git add .
-git commit -m "update readme 1499"
+git commit -m "update readme 0"
 exit 0
