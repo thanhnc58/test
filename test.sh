fd01bf81d587c25f1fbc8c70407d0d7664ea7401
thanhnc58 <nc.thanh1995@gmail.com>
2021-04-19 21:40:55 +0000

    update readme 163

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "start git"
-export GIT_AUTHOR_DATE="Mon, 19 Apr 2021 21:37:35 -0000"
-export GIT_COMMITTER_DATE="Mon, 19 Apr 2021 21:37:35 -0000"
+export GIT_AUTHOR_DATE="Mon, 19 Apr 2021 21:40:55 -0000"
+export GIT_COMMITTER_DATE="Mon, 19 Apr 2021 21:40:55 -0000"
 git add .
-git commit -m "update readme 162"
+git commit -m "update readme 163"
 exit 0
